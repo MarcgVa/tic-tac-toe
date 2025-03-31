73b2be829bf9b8335da152ea34354fefa556a979
Marc Grupe <marc.grupe@gmail.com>
2025-03-31 09:24:03 -0400

    built board using tailwinds

--- src/app.js
+++ src/app.js
@@ -1,36 +1,56 @@
-const btnOne = document.getElementById("btn-1");
+/*    Tic-Tac-Toe 
+    Operates by user loading page:
+    - user must choose to play with someone or against the computer.
+    - user must choose letter to play - The other player/computer is automatically assigned the other letter.
+    - when player clicks on tile that players letter is assigned to the tile 
+      - Tiles assigned a letter cannot be changed until board is reset.
+    - The game should announce winner when someone has three letters in a row in any direction of the board
+    - Pressing the reset clears the board and allows player one to change their letter.
+    - X always goes first
+         
+
+*/
+
+
+
 
 
 const state = {
-     'playerOne':    {
-                  'name': "",
-                  'letter':"",
-                },
-  'playerTwoLtr': {
-                  'name': "",
-                  'letter':"",
-                },
+  'playerOneLetter': "",
+  'playerTwoLetter':"",
   'playComputer': false,
-  'hasWon': false
+  'currentPlayer': "",
+  'isWinner': false
 }
 
+// Board
+const btnOne = document.getElementById("btn-1");
+const btnTwo = document.getElementById("btn-2");
+const btnThree = document.getElementById("btn-3");
+const btnFour = document.getElementById("btn-4");
+const btnFive = document.getElementById("btn-5");
+const btnSix = document.getElementById("btn-6");
+const btnSeven = document.getElementById("btn-7");
+const btnEight = document.getElementById("btn-8");
+const btnNine = document.getElementById("btn-9");
+const btnBoardReset = document.getElementById("btnReset");
+const boardButtons = [btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine];
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+const MAX_TURNS = 9;
+const turnCount = 0;
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+function SwitchTurn() { 
+  state.currentPlayer === 'x' ? state.currentPlayer = 'y' : state.currentPlayer = 'x';
+  turnCount++;
+  if (turnCount >= MAX_TURNS && !isWinner) {
+    alert('CAT has this GAME!');
   }
+  btnBoardReset.setAttribute('visible', )
 }
+
+
+btnOne.addEventListener("click", () => {
+  btnOne.innerText = state.currentPlayer;
+})
+
+
