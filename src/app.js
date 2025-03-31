06a34aa76de33ed78892aeebf87732e276d9d631
Marc Grupe <marc.grupe@gmail.com>
2025-03-30 20:27:13 -0400

    initial file commits

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const btnOne = document.getElementById("btn-1");
+
+
+const state = {
+     'playerOne':    {
+                  'name': "",
+                  'letter':"",
+                },
+  'playerTwoLtr': {
+                  'name': "",
+                  'letter':"",
+                },
+  'playComputer': false,
+  'hasWon': false
+}
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
